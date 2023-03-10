a0ce2c55c6943b4962d09b84d8e40f775da35040
Michael <fraynemichael300@gmail.com>
2023-03-09 17:28:12 -0700

    stuck on login requiring two logins

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -61,7 +61,12 @@ router.get('/details/:id', async (req, res) => {
     const post = postData.get({ plain: true });
 
     if(req.session.loggedIn) {
-      res.render('postDetails', { post, loggedIn: true });
+      const uData = await User.findByPk(req.session.userId, {
+        attributes: {
+          exclude: ['password']
+        }
+      });
+      res.render('postDetails', { post, uData: uData.toJSON(), loggedIn: true });
     } else {
       res.render('postDetails', { post, loggedIn: false });
     }
