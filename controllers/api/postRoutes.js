44a52f5aa098bbcde79e4ee3db46811e80983c05
Michael <fraynemichael300@gmail.com>
2023-03-10 23:33:57 -0700

    added comments for api routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,9 +1,11 @@
+// import required modules and models
 const router = require('express').Router();
 const { User, Post } = require('../../models');
 
 // GET all posts for homepage
 router.get('/', async (req, res) => {
   try {
+    // Find all posts and include the associated user's username
     const postData = await Post.findAll({
       include: [
         {
@@ -13,8 +15,10 @@ router.get('/', async (req, res) => {
       ],
     });
 
+    // Map post data to plain objects
     const posts = postData.map((post) => post.get({ plain: true }));
 
+    // Pass serialized data and session flag into template
     res.json({ posts, loggedIn: req.session.loggedIn });
   } catch (err) {
     res.status(500).json(err);
@@ -24,14 +28,18 @@ router.get('/', async (req, res) => {
 // POST /api/post
 router.post('/post', async (req, res) => {
   try {
+    // If post model not found, throw error
     if (!Post) {
       throw new Error('Post model not found');
     }
     req.body.userId = await req.session.userId;
+    // Create a new post
     const postData = await Post.create({
+      // spread operator to spread out the key/value pairs from the req.body object
       ...req.body,
     });
 
+    // respond with the new post data
     res.status(200).json(postData);
   } catch (err) {
     console.error(err);
@@ -42,17 +50,18 @@ router.post('/post', async (req, res) => {
 // PUT /api/post
 router.put('/post/:id', async (req, res) => {
   try {
+    // Update a post by its `id` value
     const postData = await Post.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
-
+    // If no post found, throw error
     if (!postData) {
       res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
-
+    // respond with the updated post data
     res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
@@ -62,45 +71,25 @@ router.put('/post/:id', async (req, res) => {
 // DELETE /api/post
 router.delete('/post/:id', async (req, res) => {
   try {
+    // Delete a post by its `id` value
     const postData = await Post.destroy({
       where: {
         id: req.params.id,
       },
     });
 
+    // If no post found, throw error
     if (!postData) {
       res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
 
+    // respond with the deleted post data
     res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-/* // PUT /api/post/likeCount
-router.post('/post/:id/likeCount', async (req, res) => {
-  try {
-    const likeData = await Post.update(
-      {
-        likeCount: req.body.likeCount,
-      },
-      {
-        where: {
-          id: req.params.id,
-        },
-      }
-    );
-    if (!likeData) {
-      res.status(404).json({ message: 'No post found with this id!' });
-      return;
-    }
-
-    res.status(200).json(likeData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-}); */
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,3 +1,4 @@
+// import needed modules
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const { User } = require('../../models');
@@ -6,43 +7,55 @@ const { User } = require('../../models');
 // POST /api/users
 router.post('/', async (req, res) => {
   try {
+    // Create a new user
     const dbUserData = await User.create({
       username: req.body.username,
       email: req.body.email,
       password: req.body.password
     });
 
+    // Set session variables
     req.session.userId = dbUserData.id;
     req.session.username = dbUserData.username;
     req.session.loggedIn = true;
+    // Save session variables
     req.session.save();
 
+    // Respond with the new user data
     res.json(dbUserData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// NOTE: This is how I should be doing all my response.json() calls
 // POST /api/users/login
-
 router.post('/login', async (req, res) => {
+  // email and password are sent in the request body
   const { email, password } = req.body;
   try {
+    // Find a user with the provided email address
     const userData = await User.findOne({ where: { email } });
+    // If no user found, throw error
     if (!userData) {
       return res.status(400).json({ message: 'Incorrect email or password!' });
     }
+    // Compare the provided password with the stored password
     const validPassword = await bcrypt.compare(password, userData.password);
+    // If the passwords don't match, throw error
     if (!validPassword) {
       return res.status(400).json({ message: 'Incorrect email or password!' });
     }
-    const uData = userData.toJSON();
+
+    // Map user data to plain objects
+    const uData = userData.map((user) => user.get({ plain: true }));
+    // Delete the password from the user object
     delete uData.password;
+    // Set session variables
     req.session.save(() => {
       req.session.userId = uData.id;
       req.session.username = uData.username;
       req.session.loggedIn = true;
+      // Respond with the user data
       res.json({ user: uData, message: 'You are now logged in!' });
     });
   } catch (error) {
@@ -53,6 +66,7 @@ router.post('/login', async (req, res) => {
 
 // POST /api/users/logout
 router.post('/logout', (req, res) => {
+  // If the user is logged in, destroy the session
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
