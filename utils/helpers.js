aa83874173ebca773a4dbc015816543c9936477e
Michael <fraynemichael300@gmail.com>
2023-03-10 23:47:59 -0700

    updated helper functions

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,16 +1,4 @@
 module.exports = {
-  formatDate: date => {
-    return `${ new Date(date).getMonth() + 1 }/${ new Date(date).getDate() }/${ new Date(
-      date
-    ).getFullYear() }`;
-  },
-  formatPlural: (word, amount) => {
-    if (amount !== 1) {
-      return `${ word }s`;
-    }
-
-    return word;
-  },
   ranNum: () => {
     return Math.floor(Math.random() * 1000) + 100;
   },
