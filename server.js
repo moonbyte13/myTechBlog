e245f9f43d0aa6d577d173924cb9f4aacfc5f4ff
Michael <fraynemichael300@gmail.com>
2023-03-11 23:52:19 -0700

    changed extended to true

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
